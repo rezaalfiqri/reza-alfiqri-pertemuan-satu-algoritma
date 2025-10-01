a741eabea3140f091531f42c286a30270aa56631
rezaalfiqri <rezaalfiqri001@gmail.com>
2025-10-01 10:41:31 +0700

    Add files via upload

---
+++ TUGAS KE 2 algoritma.py
@@ -0,0 +1,38 @@
+ibu_kota="jakarta"
+print(ibu_kota)
+h=1; j=2; k=h+j
+print(k)
+q=10
+if q>5:
+    print('nilai a lebih dari 5')
+a,b,c=1,2,"hebat"
+d=e=f=8
+print('d',d)
+print('f',f)
+print('e',e)
+print('a',a)
+print('b',b)
+print('c',c)
+nama_depan="reza"
+nama_belakang="alfiqri"
+nama_lengkap=nama_depan+nama_belakang
+usia="19"
+print(nama_lengkap); print('usia',usia)
+i=5
+o=10
+p=i+o
+print(p)
+nama='reza alfiqri'
+usia=24
+belum_menikah=True
+print('nama',nama)
+print('usia',usia)
+print('belum menikah',belum_menikah)
+panjang=5
+lebar=10
+luas=panjang*lebar
+print(luas)
+saya_suka_mancing=True
+saya_membaca_buku=False
+print('apakah saya suka mancing',saya_suka_mancing)
+print('apakah saya suka membaca buku',saya_membaca_buku)
