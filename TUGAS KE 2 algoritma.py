550bfadba0c2e7a88d94e6bbb96d1ce088a653e3
rezaalfiqri <rezaalfiqri001@gmail.com>
2025-10-01 09:05:15 +0700

    Add files via upload

---
+++ TUGAS KE 2 algoritma.py
@@ -0,0 +1,17 @@
+ibu_kota="jakarta"
+print(ibu_kota)
+h=1; j=2; k=h+j
+print(k)
+q=10
+if q>5:
+    print('nilai a lebih dari 5')
+a:1; b:2; c:"mantap"
+a,b,c=1,2,"mantap"
+d:10; e:10; f:10
+d=e=f=10
+print('d',d)
+print('f',f)
+print('e',e)
+print('a',a)
+print('b',b)
+print('c',c)
