8271e26caf345b93e07edd1d541abaae397c0f51
rezaalfiqri <rezaalfiqri001@gmail.com>
2026-01-07 11:09:01 +0700

    Add files via upload

---
+++ NO 1 UAS.py
@@ -0,0 +1,13 @@
+#Cara Membuat Biodata By reza
+nama = input("Masukkan nama Anda : ")
+usia = input("Masukkan usia Anda : ")
+alamat = input("Masukkan alamat Anda : ") 
+hobi = input("Masukkan hobi Anda : ")
+
+#Menampilkan biodata
+print("\n===== BIODATA DIRI =====")
+print("Nama :", nama)
+print("Usia :", usia, "tahun")
+print("Alamat :", alamat)
+print("Hobi :", hobi)
+print("=====-----------------==")
